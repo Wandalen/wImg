147813a7ea98aae95866107b8aaa80fb3e1abddb
Dmytro <dm.vict.kr@gmail.com>
2020-06-17 09:24:26 +0300

    Change structure of module, it duplicates structure of aggregated module

---
+++ proto/dwtools/amid/l3/image/include/Img.s
@@ -0,0 +1,24 @@
+( function _Img_s_()
+{
+
+'use strict';
+
+if( typeof module !== 'undefined' && module !== null )
+{
+  let _ = require( '../../../../Tools.s' );
+
+  _.include( 'wimage' );
+}
+
+let _global = _global_;
+let _ = _global_.wTools;
+let Self = _global_.wTools;
+
+// --
+// export
+// --
+
+if( typeof module !== 'undefined' && module !== null )
+module[ 'exports' ] = Self;
+
+})();
